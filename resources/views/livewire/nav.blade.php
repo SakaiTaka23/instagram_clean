2bc0cdf87c89535beb26b11e3e6d4f8b47513264
SakaiTaka23 <tt3.sakai@gmail.com>
2020-12-13 14:36:05 +0900

    logoutのアイコン追加

--- resources/views/livewire/nav.blade.php
+++ resources/views/livewire/nav.blade.php
@@ -86,6 +86,18 @@
                     </svg>
                 </a>
 
+                <form action='{{ route('logout') }}' method="POST">
+                    @csrf
+                    <a href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                                    this.closest('form').submit();">
+                        <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" stroke-linecap="round"
+                            stroke-linejoin="round" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
+                        </svg>
+                    </a>
+                </form>
+
             </div>
         </div>
     </div>
